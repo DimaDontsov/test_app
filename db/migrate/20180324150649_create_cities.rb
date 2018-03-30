56e22307cabcf4e7b8056093237983eadf5f09dc
DimaDontsov <Dimayahoo@mail.ru>
2018-03-31 00:28:44 +0300

    Исправление вылета при добавлении города в базу данных

--- db/migrate/20180324150649_create_cities.rb
+++ db/migrate/20180324150649_create_cities.rb
@@ -1,5 +1,7 @@
 class CreateCities < ActiveRecord::Migration[5.1]
   def change
+    drop_table :cities
+
     create_table :cities do |t|
       t.string :name
       t.references :region, foreign_key: true
